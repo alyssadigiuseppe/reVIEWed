4806df7c2981fdcfbf580873b1dc0ae04c3160c5
Alyssa  DiGiuseppe <alyssadigiuseppe@Alyssas-MacBook-Pro.local>
2017-04-11 18:03:38 -0400

    got the pop up working, styling changes

--- script/main.js
+++ script/main.js
@@ -1,25 +1,29 @@
 (function(){
-
+//console.log("working");
   var pic = document.querySelectorAll(".box"),
-      popup = document.querySelector("#pop"),
-      closePop = document.querySelector("#closeButton"),
-      mainImg = document.querySelector("#mainImg"),
+      pop = document.querySelector("#pop"),
+      //console.log(pop);
+      closePop = document.querySelector("#closeButton");
+      /*mainImg = document.querySelector("#mainImg"),
       desc = document.querySelector("#desc"),
-      cred = document.querySelector("#cred");
+      cred = document.querySelector("#cred");*/
 
       function open() {
-        popup.style.display = "block";
+        pop.style.display = "block";
+        pop.style.opacity = "1";
+        console.log("bruh");
       }
 
       function close(){
-        popup.style.display = "none";
-        popup.style.opacity = ".0";
+        pop.style.display = "none";
+        pop.style.opacity = 0;
       }
 
-      closePop.addEventListener('click', close, false);
+    for (var i = 0; i < pic.length; i++) {
+    pic[i].addEventListener('click', open, false);
+    console.log("listening");
+}
 
-      [].forEach.call(pic, function(selectedPic){
-        selectedPic.addEventListener('click', open, false);
-      });
+    closePop.addEventListener('click', close, false);
 
 })();
