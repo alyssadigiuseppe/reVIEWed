7c98dcc70f2ffc5d42ab4e23bdc9e97f33b7399f
Alyssa  DiGiuseppe <alyssadigiuseppe@Alyssas-MacBook-Pro.local>
2017-04-12 16:59:47 -0400

    changed thumb sizes, fixed mobile nav mostly

--- index.php
+++ index.php
@@ -23,8 +23,9 @@ if(isset($_GET['filter'])) {
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
+<meta charset="UTF-8" name="viewport" content="width=device-width"/>
 <title>reVIEWed</title>
+<link href="favicon.ico" rel="shortcut icon">
 <link href="http://fonts.googleapis.com/css?family=Mystery+Quest" rel="stylesheet" type="text/css">
 <!--<link href="css/foundation.css" rel="stylesheet" type="text/css">
 <link href="css/foundation.min.css" rel="stylesheet" type="text/css"> Needed for data interchange that isnt working-->
@@ -35,6 +36,7 @@ if(isset($_GET['filter'])) {
 <h1 class="hidden">reVIEWed - movie review site</h1>
 <div id="siteCon"> <!--Start of site Container-->
 		<nav id="mainNav">
+			<div id="sideBar">
 			<h2 class="hidden">Main navigation</h2>
 
 					<img src="images/logo.png" id="logo" alt="reVIEWed Logo">
@@ -54,47 +56,24 @@ if(isset($_GET['filter'])) {
 						</form>
 						<h6 id="copyright">Copyright © 2017</h6>
 					</ul>
-		</nav>
-
-		<section id="mobileNav">
-			<h2 class="hidden">Mobile Navigation</h2>
-			<div id="navMobileBackground">
-			<img src="images/logo.png" id="mobileLogo" alt="reVIEWed logo">
-
-					<img src="images/hamMenu.png" id="hamIcon" alt="Mobile Navigation">
 				</div>
-		</section>
-
-		<nav id="navDropdown" class="hidden">
-			<h2 class="hidden">Mobile navigation drop down menu</h2>
-			<form> <!-- action="/action_page.php" -->
-					<input class="search" type="text" placeholder="search by name" name="name"><br>
-					<select name="filter">
-						<option value="select">search by genre</option>
-						<option value="action">Action</option>
-						<option value="comedy">Comedy</option>
-						<option value="family">Family</option>
-						<option value="romance">Romance</option>
-						<option value="">All</option>
-					</select>
-					<br>
-				<input type="submit" value="Submit">
-			</form>
 		</nav>
-
 
 		<section id="page">
 	<div id="pageCon" class="welcomePage">
-		<h1 class="home">Welcome!</h1>
-		<p id="paraphrase"></p>
+		<div class="tableCon">
+			<div class="vertCon">
+		<h1 class="home"><?php if(isset($_GET['filter'])){ echo $filter; }else{ echo "Welcome!";} ?></h1>
+		<p id="paraphrase">
+		</p>
 		<div id="content"> <!-- dynamicContent goes in here -->
 			<!-- welcome page -->
-			<p id="welcomeText">Here you can search for movies by name or genre, watch trailers and read or write reviews!</p>
+			<p id="welcomeText"> <?php if(isset($_GET['filter'])){ echo ""; }else{ echo "Here you can search for movies by name or genre, watch trailers and read or write reviews.";} ?></p>
 
 			<!-- movie search-->
 			<ul class="movieCon">
 			<?php
-				if(!is_string($getMovies)){
+				if(isset($_GET['filter'])){
 					while($row = mysqli_fetch_array($getMovies)){
 						echo "
 						<li class=\"movieThumbs\">
@@ -102,10 +81,12 @@ if(isset($_GET['filter'])) {
 						</li>";
 					}
 				}else{
-					echo "<p>{$getMovies}</p>";
+					echo "<p></p>";
 				}
 			?>
 		</ul>
+	</div>
+</div>
 
 		<div id="pop">
 		  <img src="images/closeButton.svg" alt="close full image button" id="closeButton" title="esc key">
@@ -126,6 +107,8 @@ if(isset($_GET['filter'])) {
 
 		</div><!-- dynamicContent Finishes -->
 	</div>
+
+<h6 id="mobileCopyright">Copyright © 2017</h6>
 
 </section>
 </div><!--End of site container-->
